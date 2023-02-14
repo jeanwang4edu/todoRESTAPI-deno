521af4be1df93664c5b8badd7fc891b1e52bb24b
jeanwang4edu <tocoins@gmail.com>
2023-02-14 14:47:46 -0500

    add middleware

--- app.ts
+++ app.ts
@@ -4,7 +4,13 @@ import todoRoutes from "./routes/todos.ts";
 
 const app = new Application();
 
+app.use( async (ctx, next) => {
+    console.log('Middleware!');
+    await next();
+})
+
 //https://deno.land/x/oak@v11.1.0/mod.ts?s=Router#method_routes_0
 app.use(todoRoutes.routes());
+app.use(todoRoutes.allowedMethods());
 
 await app.listen({ port: 3000 });
